90e70f706b058e5250b934a969d62bde500b21ba
Alejandro Ramírez <81694980+lexrammart@users.noreply.github.com>
2026-03-14 21:19:40 -0600

    Function showUpdateNotification created on ui.js

--- frontend/javascript/ui.js
+++ frontend/javascript/ui.js
@@ -179,3 +179,14 @@ function updateUI(d) {
   updatePedal("bar-brake", "val-brake", d.fren);
   updatePedal("bar-throttle", "val-throttle", d.acel);
 }
+
+function showUpdateNotification(data){
+  const version = data.version;
+  const notes = data.changelog || "Mejoras generales de MATI.";
+
+  const userChoice = confirm(`Nueva versión de MATI disponible: ${version}!\n\nNotas:\n${notes}\n\n¿Quieres actualizar?`);
+
+  if (userChoice){
+    window.pywebview.api.open_drive_folder();
+  }
+}
