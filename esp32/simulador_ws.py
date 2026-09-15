b238f10024b631d3ab96574c43d7e58a660d998e
Alejandro Ramírez <81694980+lexrammart@users.noreply.github.com>
2026-09-15 02:45:57 -0600

    Fix websocket handler and ensure proper server initialization

--- esp32/simulador_ws.py
+++ esp32/simulador_ws.py
@@ -4,13 +4,14 @@ import websockets
 
 async def handler():
     try:
-        await websocket.await_closed()
+        await websockets.await_closed()
     except websockets.exceptions.ConnectionClosedOK:
         pass
 
 
 async def main():
-    async with websockets.serve(handler, "0.0.0.0", 81)
+    async with websockets.serve(handler, "0.0.0.0", 81):
+        await asyncio.Future()
 
 
 if __name__ == "__main__":
